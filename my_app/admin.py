12926dcb1fec462f44cb73bdf3f9f151ea249e70
katerynalol <katua.korzyn@gmail.com>
2026-03-05 18:19:29 +0100

    homework(8): register new models(Task, SubTask, Category) in admin.py

--- my_app/admin.py
+++ my_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from my_app.models import *
 
-# Register your models here.
+
+admin.site.register(Task)
+admin.site.register(SubTask)
+admin.site.register(Category)
