df04ca2e0790302400e1e4d1c950d03f539fc61f
katerynalol <katua.korzyn@gmail.com>
2026-03-10 17:59:20 +0100

    homework(11): admin: improve UI with inlines, custom actions, and title truncation

--- my_app/admin.py
+++ my_app/admin.py
@@ -2,10 +2,14 @@ from django.contrib import admin
 from my_app.models import *
 
 
+class SubTaskInline(admin.StackedInline):
+    model = SubTask
+
+
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
     list_display = [
-        "title",
+        "short_title",
         "status",
         "deadline"
     ]
@@ -18,6 +22,16 @@ class TaskAdmin(admin.ModelAdmin):
         "status"
     ]
     list_per_page = 10
+    inlines = [
+        SubTaskInline
+    ]
+    def short_title(self, obj):
+        if len(obj.title) > 10:
+            return obj.title[:10]+'...'
+        return obj.title
+
+    short_title.short_description = "Название"
+    short_title.admin_order_field = "title"
 
 
 @admin.register(SubTask)
@@ -37,6 +51,13 @@ class SubTaskAdmin(admin.ModelAdmin):
         "status"
     ]
     list_per_page = 10
+
+    actions = ["mark_as_done"]
+    def mark_as_done(self, request, queryset):
+        queryset.update(status="Done")
+        self.message_user(request, "Выбранные подзадачи переведены в статус Done")
+    mark_as_done.short_description = "Перевести выбранные подзадачи в Done"
+
 
 
 @admin.register(Category)
