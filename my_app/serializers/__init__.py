0c7ea81f8aa051790299a44f823d592479470d23
katerynalol <katua.korzyn@gmail.com>
2026-03-26 23:10:45 +0100

    homework(13): add SubTaskCreateSerializer

--- my_app/serializers/__init__.py
+++ my_app/serializers/__init__.py
@@ -1,5 +1,8 @@
 from my_app.serializers.task import TaskSerializer
+from my_app.serializers.subtask import SubTaskCreateSerializer
+
 
 __all__ = [
     "TaskSerializer",
+    "SubTaskCreateSerializer",
 ]
