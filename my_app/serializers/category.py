aea3d89c5cf8c276abc60fc15c557cc10ee64c72
katerynalol <katua.korzyn@gmail.com>
2026-03-26 23:07:11 +0100

    homework(13):Add CategoryCreateSerializer with unique name validation for create and update methods

---
+++ my_app/serializers/category.py
@@ -0,0 +1,39 @@
+# Задание 2: Переопределение методов create и update
+# Создайте сериализатор для категории CategoryCreateSerializer,
+# переопределив методы create и update для проверки уникальности названия категории.
+# Если категория с таким названием уже существует, возвращайте ошибку валидации.
+# Шаги для выполнения:
+# Определите CategoryCreateSerializer в файле serializers.py.
+# Переопределите метод create для проверки уникальности названия категории.
+# Переопределите метод update для аналогичной проверки при обновлении.
+
+from typing import Any
+from rest_framework import serializers
+from my_app.models import Category
+
+
+class CategoryCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+
+    def create(self, validated_data: dict[str, Any]) -> Category:
+        name_category = Category.objects.filter(
+            name=validated_data['name']).exists()
+        if name_category:
+            raise serializers.ValidationError(
+                "Такая категория уже существует")
+        return Category.objects.create(**validated_data)
+
+    def update(self, instance, validated_data) -> Category:
+        name_category = Category.objects.filter(
+            name=validated_data['name']
+        ).exclude(id=instance.id).exists()
+        if name_category:
+            raise serializers.ValidationError(
+                "Такая категория уже существует"
+            )
+        instance.name = validated_data['name']
+        instance.save()
+        return instance
